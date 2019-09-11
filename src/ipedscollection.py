6e34e10b823c159b5363ac2ab0509fcf1e46f2b9
fberendse0710 <fbb0710@gmail.com>
2019-09-11 13:52:50 -0600

    meta attribute added to IpedsCollection

--- src/ipedscollection.py
+++ src/ipedscollection.py
@@ -1,32 +1,58 @@
+from imputationtypes import ImputationTypes
 from ipedstable import IpedsTable
 
 class IpedsCollection(object):
     ''' A collection of multiple IpedsTables '''
     
 
-    def __init__(self, table_dict = {}):
-        self.names = []
-        for name,table in table_dict.items():
-            self.add(name,table)
+    def __init__(self):
+        self.meta = {}
         return
 
-    def add(self,name,table):
+    def add_table(self,name,table):
         if not isinstance(table,IpedsTable):
             raise TypeError("table must be an instance of IpedsTable.")
         setattr(self,name,table)
-        self.names.append(name) 
         return
 
-    def add_from_file(self,filepath):
-        pass
+    def add_table_from_file(self,name,filepath):
+        table = IpedsTable(filepath=filepath)
+        setattr(self,name,table)
+        return
+
+    def update_meta(
+            self,
+            name,
+            table=None,
+            filepath=None,
+            keep_columns=None,
+            exclude_imputations=None):
+        
+        if name not in self.meta.keys():
+            self.meta.update({name:{}})
+        entry = self.meta[name]
+        
+        if table:
+            entry.update({'table':table})
+        
+        if filepath:
+            entry.update({'filepath':filepath})
+                
+        if keep_columns:
+            entry.update({'keep_columns':keep_columns}) 
+            
+        if exclude_imputations:
+            entry.update({'exclude_imputations':exclude_imputations})
+        return
 
     def drop(self,name):
-        try:
-            delattr(self,name)
-            self.names.remove(name)
-        except:
-            raise NameError('table name not found')
+        delattr(self,name)
+        del self.meta[name]
         return 
+
+    def import_tables(self):
+        #import tables in the meta that are not currently in the namesapce
+        pass
 
     def keep_columns(self):
         # slices each table to its list of keep_columns
@@ -46,7 +72,7 @@ class IpedsCollection(object):
         pass
 
     def join_all(self):
-        for i,name in enumerate(self.names):
+        for i,name in enumerate(self.meta.keys()):
             table = getattr(self,name)
             if i == 0:
                 merged_df = table.df
@@ -54,3 +80,37 @@ class IpedsCollection(object):
                 merged_df = merged_df.join(table.df,rsuffix=name)
 
         return IpedsTable(df=merged_df)
+
+
+if __name__ == '__main__':
+    
+    exclude_list = [
+            ImputationTypes.data_not_usable,
+            ImputationTypes.do_not_know,
+            ImputationTypes.left_blank,
+            ImputationTypes.not_applicable
+            ]
+
+    adm2017 = IpedsTable(filepath='data/adm2017.csv')
+    hd2017 = IpedsTable(filepath='data/hd2017.csv')
+    column_list = ['enrlpt']
+    adm2017.drop_imputations(exclude_list,column_list,how='any')
+    adm2017.write_csv('data/test.csv')
+
+    tc = IpedsCollection()
+    tc.update_meta(
+            'adm2017',
+            table=adm2017,
+            keep_columns='all',
+            exclude_imputations=exclude_list)
+    tc.update_meta(
+            'hd2017',
+            table=hd2017,
+            keep_columns=['instnm','city','stabbr'],
+            exclude_imputations=exclude_list)
+    tc.update_meta(
+            'gr2017',
+            filepath='data/gr2017.csv',
+            keep_columns='all',
+            exclude_imputations=exclude_list)
+   # merged_table = tc.join_all()
