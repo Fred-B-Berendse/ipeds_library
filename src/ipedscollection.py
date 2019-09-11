95c9d52670a13cd7df235d31e37a304f319afb29
fberendse0710 <fbb0710@gmail.com>
2019-09-11 09:53:15 -0600

    blank keep_columns, purge_imputations, dropna_all

--- src/ipedscollection.py
+++ src/ipedscollection.py
@@ -5,19 +5,20 @@ class IpedsCollection(object):
     
 
     def __init__(self, table_dict = {}):
-
         self.names = []
         for name,table in table_dict.items():
             self.add(name,table)
         return
 
     def add(self,name,table):
-
         if not isinstance(table,IpedsTable):
             raise TypeError("table must be an instance of IpedsTable.")
         setattr(self,name,table)
         self.names.append(name) 
         return
+
+    def add_from_file(self,filepath):
+        pass
 
     def drop(self,name):
         try:
@@ -27,8 +28,24 @@ class IpedsCollection(object):
             raise NameError('table name not found')
         return 
 
+    def keep_columns(self):
+        # slices each table to its list of keep_columns
+        pass
+
+    def purge_imputations(self):
+        # removes excepted imputations from all tables
+        pass
+
+    def dropna_all(self):
+        # drops NaN values in all columns in each table
+        pass
+
+    def clean_all(self):
+        # runs keep_columns, purge_imputations, dropna_all 
+        # on all tables
+        pass
+
     def join_all(self):
-        
         for i,name in enumerate(self.names):
             table = getattr(self,name)
             if i == 0:
