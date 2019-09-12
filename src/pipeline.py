e54b5265b5504696faec3f68328a3345855a057b
fberendse0710 <fbb0710@gmail.com>
2019-09-11 23:15:35 -0600

    test pipeline completed

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,48 +1,59 @@
 from imputationtypes import ImputationTypes as it
 from ipedscollection import IpedsCollection
-from dbpipeline import DatabasePipeline
+#from dbpipeline import DatabasePipeline
+from ipedsdatabase import IpedsDatabase
 
-# exclude_list = [
-#         it.data_not_usable,
-#         it.do_not_know,
-#         it.left_blank,
-#         it.not_applicable
-#         ]
+exclude_list = [
+        it.data_not_usable,
+        it.do_not_know,
+        it.left_blank,
+        it.not_applicable
+        ]
 
-# tc = IpedsCollection()
-# tc.update_meta(
-#         'hd2017',
-#         filepath='data/hd2017.csv',
-#         keep_columns=['instnm','city','stabbr'],
-#         exclude_imputations=exclude_list)
-# tc.update_meta(
-#         'adm2017',
-#         filepath='data/adm2017.csv',
-#         keep_columns='all',
-#         exclude_imputations=exclude_list)
-# tc.update_meta(
-#         'gr2017',
-#         filepath='data/gr2017.csv',
-#         keep_columns='all',
-#         exclude_imputations=exclude_list)
+tc = IpedsCollection()
+tc.update_meta(
+        'hd2017',
+        filepath='data/hd2017.csv',
+        keep_columns=['instnm','city','stabbr'],
+        exclude_imputations=exclude_list)
+tc.update_meta(
+        'adm2017',
+        filepath='data/adm2017.csv',
+        keep_columns='all',
+        exclude_imputations=exclude_list)
+tc.update_meta(
+        'gr2017',
+        filepath='data/gr2017.csv',
+        keep_columns='all',
+        exclude_imputations=exclude_list)
 
-# tc.clean_tables()
-# merged_table = tc.join_all()
+tc.clean_tables()
+merged_table = tc.join_all()
 
-print("attempting connection")
-dp = DatabasePipeline('ipeds','postgres','localhost','5435')
-print("Created DB Pipeline Object")
-dp.add_step("SELECT datname FROM pg_database where datistemplate=false;")
-dp.execute()
-print("Executed check")
-print(f'Row count: {dp.c.rowcount}')
-for row in dp.c:
-    print(row)
-dp.close()
+ipdb = IpedsDatabase('localhost','5435','postgres','ipeds')
+ipdb.to_sql(merged_table,'data_2017')
 
-# cur.execute(sql.SQL("CREATE DATABASE {}").format(
-#         sql.Identifier(self.db_name))
+sqlstr = "SELECT unitid,instnm,city,stabbr FROM data_2017 LIMIT 5;"
+result_set = ipdb.execute(sqlstr)
+for r in result_set:
+    print(r)
 
-# SELECT table_schema,table_name
-# FROM information_schema.tables
-# ORDER BY table_schema,table_name;
+ipdb.close()
+
+
+# print("attempting connection")
+# dp = DatabasePipeline('ipeds','postgres','localhost','5435')
+# print("Created DB Pipeline Object")
+# dp.add_step("SELECT datname FROM pg_database where datistemplate=false;")
+# dp.execute()
+# print("Executed check")
+# print(f'Row count: {dp.c.rowcount}')
+# for row in dp.c:
+#     print(row)
+# dp.close()
+
+
+# sqlstr = "SELECT datname FROM pg_database where datistemplate=false;"
+# result_set = ipdb.engine.execute(sqlstr)
+# for r in result_set:
+#     print(r)
