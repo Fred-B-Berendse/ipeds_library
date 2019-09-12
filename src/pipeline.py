c8e3aeba30478d4a728a9bfbd0091477fb8a2294
fberendse0710 <fbb0710@gmail.com>
2019-09-11 22:05:39 -0600

    database testing

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,32 +1,48 @@
 from imputationtypes import ImputationTypes as it
 from ipedscollection import IpedsCollection
-from dbpipeline import DatabasePipeline as dp
+from dbpipeline import DatabasePipeline
 
-if __name__ == '__main__':
-    
-    exclude_list = [
-            it.data_not_usable,
-            it.do_not_know,
-            it.left_blank,
-            it.not_applicable
-            ]
+# exclude_list = [
+#         it.data_not_usable,
+#         it.do_not_know,
+#         it.left_blank,
+#         it.not_applicable
+#         ]
 
-    tc = IpedsCollection()
-    tc.update_meta(
-            'hd2017',
-            filepath='data/hd2017.csv',
-            keep_columns=['instnm','city','stabbr'],
-            exclude_imputations=exclude_list)
-    tc.update_meta(
-            'adm2017',
-            filepath='data/adm2017.csv',
-            keep_columns='all',
-            exclude_imputations=exclude_list)
-    tc.update_meta(
-            'gr2017',
-            filepath='data/gr2017.csv',
-            keep_columns='all',
-            exclude_imputations=exclude_list)
+# tc = IpedsCollection()
+# tc.update_meta(
+#         'hd2017',
+#         filepath='data/hd2017.csv',
+#         keep_columns=['instnm','city','stabbr'],
+#         exclude_imputations=exclude_list)
+# tc.update_meta(
+#         'adm2017',
+#         filepath='data/adm2017.csv',
+#         keep_columns='all',
+#         exclude_imputations=exclude_list)
+# tc.update_meta(
+#         'gr2017',
+#         filepath='data/gr2017.csv',
+#         keep_columns='all',
+#         exclude_imputations=exclude_list)
 
-    tc.clean_tables()
-    merged_table = tc.join_all()
+# tc.clean_tables()
+# merged_table = tc.join_all()
+
+print("attempting connection")
+dp = DatabasePipeline('ipeds','postgres','localhost','5435')
+print("Created DB Pipeline Object")
+dp.add_step("SELECT datname FROM pg_database where datistemplate=false;")
+dp.execute()
+print("Executed check")
+print(f'Row count: {dp.c.rowcount}')
+for row in dp.c:
+    print(row)
+dp.close()
+
+# cur.execute(sql.SQL("CREATE DATABASE {}").format(
+#         sql.Identifier(self.db_name))
+
+# SELECT table_schema,table_name
+# FROM information_schema.tables
+# ORDER BY table_schema,table_name;
