b75dd6ab799625c70eff0b9f95a0c9d6b1924c7e
fberendse0710 <fbb0710@gmail.com>
2019-09-12 15:12:25 -0600

    pipeline_all created

--- src/ipedscollection.py
+++ src/ipedscollection.py
@@ -1,13 +1,13 @@
+import pandas as pd
 from ipedstable import IpedsTable
-from copy import deepcopy
-import gc
+from copy import copy,deepcopy
 
 class IpedsCollection(object):
     ''' A collection of multiple IpedsTables '''
     
-
     def __init__(self):
         self.meta = {}
+        self.merged_table = IpedsTable(df=pd.DataFrame())
         return
 
     def update_meta(
@@ -57,38 +57,54 @@ class IpedsCollection(object):
             del entry['filepath']
         return
 
-    def clean_tables(self,dropna=False):
+    def clean_all(self,dropna=False):
         for name in self.meta.keys():
-            print(f"processing table {name}")
+            self.clean_table(name,dropna=dropna)
+        return 
+
+    def clean_table(self,name,dropna=False):
+        print(f"cleaning table {name}")
+        self.import_table(name)
+        entry = self.meta[name]
+        table = entry['table']
+        if entry['keep_columns'] == 'all':
+            entry['keep_columns'] = table.columns
+        table.keep_columns(entry['keep_columns'])
+        table.purge_imputations(entry['exclude_imputations'])
+        if dropna:
+            table.dropna(entry['keep_columns'],how='any')
+        return
+
+    def merge_table(self,name,how='outer',keep_table=True):
+        print(f"Merging {name} with merged_table")
+        self._validate_table_import(name)
+        table = self.meta[name]['table']
+        if len(self.merged_table) == 0:
+            self.merged_table.df = table.df.copy()
+        else:
+            self.merged_table.df = self.merged_table.df.merge(
+                                table.df,
+                                how=how,
+                                on='unitid',
+                                suffixes=('','_'+name))
+        if not keep_table: 
+            self.drop_meta(name)
+        return
+
+    def _validate_table_import(self,name):
+        if not 'table' in self.meta[name].keys():
+            raise KeyError('Table has not been imported.')
+        return 
+
+    def merge_all(self,how='inner',keep_table=True):
+        for name in self.meta.keys():
+            self.merge_table(name,how=how,keep_table=keep_table)
+        return
+
+    def pipeline_all(self,dropna=False,how='inner',keep_table=True):
+        names = list(self.meta.keys())
+        for name in names:
             self.import_table(name)
-            entry = self.meta[name]
-            table = entry['table']
-            if entry['keep_columns'] == 'all':
-                entry['keep_columns'] = table.columns
-            table.keep_columns(entry['keep_columns'])
-            table.purge_imputations(entry['exclude_imputations'])
-            if dropna:
-                table.dropna(entry['keep_columns'],how='any')
-        return 
-
-    def join_all(self):
-        self._validate_table_imports()
-        merged_df = None
-        for name,entry in self.meta.items():
-            print(f'Merging {name}')
-            table = entry['table']
-            if merged_df is None:
-                merged_df = table.df.copy()
-            else:
-                merged_df = merged_df.merge(
-                        table.df,
-                        how='outer',
-                        on='unitid',
-                        suffixes=('','_'+name))
-            gc.collect()
-        return IpedsTable(df=merged_df)
-
-    def _validate_table_imports(self):
-        if not all(['table' in entry.keys() for entry in self.meta.values()]):
-            raise KeyError('One or more tables has not been imported.')
-        return 
+            self.clean_table(name,dropna=dropna)
+            self.merge_table(name,how=how,keep_table=keep_table)
+        return
